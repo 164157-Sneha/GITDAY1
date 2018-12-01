f22687d3a71efee5f424584d7b8f2864e9433c05
164157-Sneha <44638963+164157-Sneha@users.noreply.github.com>
2018-12-01 15:59:10 +0530

    Add files via upload

--- src/com/sneha/asgn/Departments.java
+++ src/com/sneha/asgn/Departments.java
@@ -16,7 +16,7 @@ public class Departments {
         emp.add(new Emplo(124,"Zehara",30,"IMS",3000,"President"));
         emp.add(new Emplo(176,"Shruthi",20,"BIU",3000,"Manager"));
         emp.add(new Emplo(144,"Siya",30,"IMS",1500,"Salesman"));
-        emp.add(new Emplo(198,"Tanya",20,"BIU",1500,"salesman"));
+        emp.add(new Emplo(198,"Tanya",20,"BIU",1500,"Salesman"));
         emp.add(new Emplo(161,"Varshitha",20,"BIU",1200,"Clerk"));
         emp.add(new Emplo(182,"Vidya",30,"IMS",3500,"Manager"));
         
@@ -93,12 +93,45 @@ public class Departments {
 	    .forEach(System.out::println);
 	    System.out.println("\n");
 	    
-	    System.out.println("10. Managers and clkerks in dept 10");
+	    System.out.println("10. Managers and clerks in dept 10");
 	    emp.stream()
 	    .filter(m->((m.getPosition().equals("Manager") || m.getPosition().equals("Clerk")) && m.getDeptno()==10))
 	    		.map(x->x.getEmpname())
 	    		.forEach(System.out::println);
+	    System.out.println("\n");
 	    
+	    System.out.println("11.Employees sorted according to names");
+	    emp.stream()
+	    .map(n->n.getEmpname())
+	    .sorted() 
+	    .forEach(System.out::println);
+	    System.out.println("\n");
+	    
+	    System.out.println("12. Managers in dept 10 and clerks in dept 20");
+	    emp.stream()
+	       .filter(n->((n.getPosition().equals("Manager") && n.getDeptno()==10) || (n.getPosition().equals("Clerk") && n.getDeptno()==20)))
+	       .map(z->z.getEmp_id() + " " + z.getEmpname())
+	       .forEach(System.out::println);
+	    System.out.println("\n");
+	    
+	    System.out.println("13. Employees earning between 1200 and 1400 are:");
+	    emp.stream()
+	    .filter((a->a.getSalary()>=1200 && a.getSalary()<=1400 ))
+	    .map(a->a.getEmp_id()+ " "+ a.getEmpname()+" "+a.getSalary())
+	    .forEach(System.out::println);
+	    System.out.println("\n");
+	    
+	    System.out.println("14. Clerks,Salesman and Analyst");
+	    emp.stream()
+	    .filter(b->(b.getPosition().equals("Clerk") || b.getPosition().equals("Salesman") || b.getPosition().equals("Analyst")))
+	    .map(s->s.getEmpname())
+	    .forEach(System.out::println);
+	    System.out.println("\n");
+	    
+	    System.out.println("15. Replace character");
+	    emp.stream()
+	    .map(n->n.getEmpname().replace('a', 'e'))
+	    .forEach(System.out::println);
 	}
 
 }
